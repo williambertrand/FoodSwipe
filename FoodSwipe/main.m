3ad834e616850f137dbbe072b2ed6855c066265b
William Bertrand <William@nat-oitwireless-inside-vapornet100-c-5042.Princeton.EDU>
2015-01-04 00:50:51 -0500

    Initial Commit

---
+++ FoodSwipe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoodSwipe
+//
+//  Created by William Bertrand on 1/4/15.
+//  Copyright (c) 2015 William Bertrand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
